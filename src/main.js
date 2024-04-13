8bec25949963e827637b2560e5c8ef2626164ccd
aplaza2 <aplaza2@uc.cl>
2024-04-12 23:21:36 -0400

    Muestra tiempos de ejecucion

--- src/main.js
+++ src/main.js
@@ -7,16 +7,33 @@ document.addEventListener('DOMContentLoaded', () => {
 
     calculateButton.addEventListener('click', async () => {
         const input = document.getElementById("input").valueAsNumber;
+        let start = performance.now();
         let resultCO2 = await O2WasmPrimeFactors(input);
+        const CO2Time = performance.now() - start;
+
+        start = performance.now();
         let resultCO3 = await O3WasmPrimeFactors(input);
+        const CO3Time = performance.now() - start;
+
+        start = performance.now();
         let resultJs = await jsPrimeFactors(input);
+        const JsTime = performance.now() - start;
+
         displayResults("resultCO2", resultCO2);
         displayResults("resultCO3", resultCO3);
         displayResults("resultJs", resultJs);
+
+        displayTime("timeCO2", CO2Time);
+        displayTime("timeCO3", CO3Time);
+        displayTime("timeJs", JsTime);
     });
 });
 
 
 function displayResults(resutlId, factors) {
   document.getElementById(resutlId).innerText = "El resultado es: " + factors.join(", ");
+}
+
+function displayTime(timeId, time) {
+  document.getElementById(timeId).innerText = "El tiempo de ejecución es: " + time + " ms";
 }
